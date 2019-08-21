f5007a5ad509dc59cfdf0a1e0b3dbfc67d56468a
Alexander <alexslabkin@gmail.com>
2019-08-21 17:19:11 +0300

    add main file itob.py

--- domenrequest.py
+++ domenrequest.py
@@ -1,6 +1,17 @@
 import requests
+from bs4 import BeautifulSoup
 
 r = requests.get('https://vk.com')
+soup = BeautifulSoup(r.text, 'html.parser')
 print(r.text)
+print(soup.prettify())
 
+def parse():
+    title = soup.title.string
+    keywords = ''
+    for keyword in soup.find_all('keywords'):
+        keywords += keyword + ' '
+    description = soup.description.string
+
+    return url, title, description, keywords
 
