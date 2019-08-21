a2b5d49e9ec245aea6336fd44e31b75844e79e24
Alexander <alexslabkin@gmail.com>
2019-08-21 17:23:48 +0300

    fix errors

--- sqltables.py
+++ sqltables.py
@@ -1,12 +1,15 @@
-from sqlalchemy import Column, ForeignKey, Integer, String
-from sqlalchemy.ext.daclarative import declarative_base
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from datetime import datetime
 
+Base = declarative_base()
+
 class InfoSite(Base):
+    __tablename__ = 'infosite'
     id = Column(Integer, primary_key=True)
     url = Column(String(250), nullable=False)
-    title = Column(String(250))
+    title = Column(String(250), index=True)
     description = Column(String(250))
     keywords = Column(String(250))
     date_of_operation = Column(DateTime, index=True, default=datetime.utcnow)
