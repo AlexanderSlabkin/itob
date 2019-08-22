977d47890e2730fd57d84985a318172c5388ceb0
Alexander <alexslabkin@gmail.com>
2019-08-22 22:23:37 +0300

    relation: one to many, implement as Class with option to check only new instances in Domen

---
+++ alembic/versions/e4af5558d6d8_change_relations_for_one_domen_to_many_.py
@@ -0,0 +1,24 @@
+"""change relations for one domen to many sites
+
+Revision ID: e4af5558d6d8
+Revises: 1f1f5be3cc67
+Create Date: 2019-08-22 22:16:39.232662
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'e4af5558d6d8'
+down_revision = '1f1f5be3cc67'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
