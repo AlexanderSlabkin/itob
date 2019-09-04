4e5905645f83441bdbd4f0434e31015afeafb066
Alexander <alexslabkin@gmail.com>
2019-09-04 15:40:34 +0300

    Realizq parsing of description

--- domenrequest.py
+++ domenrequest.py
@@ -1,24 +1,30 @@
+#!/usr/bin/env python3
 import requests
 from bs4 import BeautifulSoup
 
-r = requests.get('https://vk.com')
-soup = BeautifulSoup(r.text, 'html.parser')
-#print(r.text)
-#print(soup.prettify())
 
 def parse(domen):
+
     url = 'https://'+domen
     r = requests.get(url)
     soup = BeautifulSoup(r.content, 'html.parser')
     title = ''
     if soup.title:
         title = soup.title.get_text()
+    title = soup.find('title').get_text()
     keywords = ''
     for keyword in soup.find_all('keywords'):
-        keywords += keyword + ' '
-    description = ''
-    if soup.description:
-        description = soup.description.get_text()
-
+        keywords += keyword.string + ' '
+    description = soup.find(attrs={'name': 'description'})
+    if description: description = description.get('content')
     return url, title, description, keywords[:-1]
+
+
+if __name__ == '__main__':
+    domen = 'ok.ru'
+    p = parse(domen)
+    r = requests.get('https://' + domen)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    #print(soup.prettify())
+    print(p)
 
